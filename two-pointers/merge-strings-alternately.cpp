82e905b02a73bd92e9f38ef4d3ea7829ed6a350f
Namita <namitasharma1124@gmail.com>
2026-08-03 23:01:45 +0530

    LC-merge-strings-alternately [Runtime: 4 ms; Memory: 8.3 MB]

--- two-pointers/merge-strings-alternately.cpp
+++ two-pointers/merge-strings-alternately.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
         string ans="";
-        int i = 0;
-        while (i < word1.size() || i < word2.size()) {
-            if (i < word1.size())
-                ans += word1[i];
-            if (i < word2.size())
-                ans += word2[i];
-            i++;
+           int i = 0;
+        int j = 0;
+        while (i < word1.size() && j < word2.size()) {
+            ans += word1[i++];
+            ans += word2[j++];
         }
+        while (i < word1.size())
+            ans += word1[i++];
+        while (j < word2.size())
+            ans += word2[j++];
         return ans;
     }
 };
