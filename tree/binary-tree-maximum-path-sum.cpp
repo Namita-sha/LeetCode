7e6f78b3edb88e649cdcb87f7612345fa29ed3ca
Namita <namitasharma1124@gmail.com>
2026-08-06 07:23:45 +0530

    LC-binary-tree-maximum-path-sum [Runtime: N/A; Memory: N/A]

--- tree/binary-tree-maximum-path-sum.cpp
+++ tree/binary-tree-maximum-path-sum.cpp
@@ -1,33 +1,15 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int maxPathSum(TreeNode* root) {
-        int res = root->val;
-        dfs(root, res);
-        return res;
+         int maxi=INT_MIN;
+         maxPathDown(root,maxi);
+         return maxi;
     }
-
-private:
-    int dfs(TreeNode* node, int& res) {
-        if (!node) {
-            return 0;
-        }
-
-        int leftSum = max(0, dfs(node->left, res));
-        int rightSum = max(0, dfs(node->right, res));
-
-        res = max(res, leftSum + rightSum + node->val);
-
-        return max(leftSum, rightSum) + node->val;
+    int maxPathDown(TreeNode* node,int & maxi){
+        if(node==NULL) return 0;
+        int left=max(0,maxPathDown(node->left,maxi));
+        int right=max(0,maxPathDown(node->right,maxi));
+        maxi=max(maxi,left+right+node->val);
+        return max(left,right)+node->val;
     }
 };
