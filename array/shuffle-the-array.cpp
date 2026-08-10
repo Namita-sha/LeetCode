1dd4010c03fe38c920d9a42316dc3382e375592e
Namita <namitasharma1124@gmail.com>
2026-08-10 22:57:40 +0530

    LC-shuffle-the-array [Runtime: N/A; Memory: N/A]

---
+++ array/shuffle-the-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+         int[] result = new int[2 * n];
+
+        for (int i = 0; i < n; i++) {
+            result[2 * i] = nums[i];
+            result[2 * i + 1] = nums[n + i];
+        }
+
+        return result;
+    }
+};
