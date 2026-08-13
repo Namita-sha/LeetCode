c5f381c4d0ecaf32f127377cf8c7f4229a802366
Namita <namitasharma1124@gmail.com>
2026-08-13 10:14:44 +0530

    LC-isomorphic-strings [Runtime: N/A; Memory: N/A]

--- hash-table/isomorphic-strings.cpp
+++ hash-table/isomorphic-strings.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-   int arr1[256]={0},arr2[256]={0};
-   int n=s.size(); // only s as both should have same size
-   for(int i=0;i<n;i++){
-    if(arr1[s[i]]!=arr2[t[i]]){
-        return false;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+           if(s[j]==s[i]&&t[j]!=t[i]){
+            return false;
+           }
+           if(t[j]==t[i]&&s[j]!=s[i]){
+            return false;
+           }
+            }
+        }
+        return true;
     }
-        arr1[s[i]]=i+1; //These characters were last seen at index i
-        arr2[t[i]]=i+1;
-    
-   }
-   return true;
-}
 };
