c5ba38a5b6ba00d6d491b282bce4a50cbb1da2d5
Namita <namitasharma1124@gmail.com>
2026-08-15 12:19:33 +0530

    LC-subarray-sum-equals-k [Runtime: N/A; Memory: N/A]

--- prefix-sum/subarray-sum-equals-k.cpp
+++ prefix-sum/subarray-sum-equals-k.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n=nums.size();
-       
         int count=0;
         for(int i=0;i<n;i++){
-             int sum=0;
             for(int j=i;j<n;j++){
-                sum+=nums[j];
-               
-                if(sum==k){
-                     count++;  
-                }
-                
+                int sum=0;
+                for(int x=i;x<=j;x++){
+                    sum+=nums[x];                
+            }
+            if(sum==k){
+                count++;
             }
         }
+    }
         return count;
     }
 };
