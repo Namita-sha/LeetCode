6c4551845dc7087c42e490489ecde7ff9c639ca8
Namita <namitasharma1124@gmail.com>
2026-08-15 11:58:35 +0530

    LC-range-sum-query-immutable [Runtime: N/A; Memory: N/A]

--- design/range-sum-query-immutable.cpp
+++ design/range-sum-query-immutable.cpp
@@ -1,16 +1,19 @@
 class NumArray {
+    vector<int> prefix;
 public:
-vector<int> nums;
+
     NumArray(vector<int>& nums) {
-        this->nums=nums;
+       int n=nums.size();
+       
+       prefix.resize(n+1,0);
+       for(int i=0;i<n;i++){
+        prefix[i+1]=prefix[i]+nums[i];
+       }
     }
     
     int sumRange(int left, int right) {
-        int sum=0;
-        for(int i=left;i<=right;i++){
-            sum+=nums[i];
-        }
-      return sum;  
+       
+        return prefix[right+1]-prefix[left];
     }
 };
 
