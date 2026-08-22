9c8a7bb850fb79596685f9fe8f562604af7bb533
Namita <namitasharma1124@gmail.com>
2026-08-22 22:52:31 +0530

    LC-palindromic-substrings [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/palindromic-substrings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+         int res = 0;
+         int n=s.size();
+        for (int i = 0; i <n; i++) {
+            for (int j = i; j <n; j++) {
+                int l = i, r = j;
+                while (l < r && s[l] == s[r]) {
+                    l++;
+                    r--;
+                }
+                res += (l >= r);
+            }
+        }
+
+        return res;
+    }
+};
